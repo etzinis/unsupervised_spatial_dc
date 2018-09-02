3a625832c41f51ada28740aee7405ee878ae5a62
Efthymios Tzinis <etzinis2@illinois.edu>
2018-09-02 18:04:02 -0500

    Pytorch utils for dataset creatiuon and other things as well

---
+++ spatial_two_mics/pytorch_utils/dataset.py
@@ -0,0 +1,6 @@
+"""!
+@brief A dataset creation which is compatible with pytorch framework
+
+@author Efthymios Tzinis {etzinis2@illinois.edu}
+@copyright University of illinois at Urbana Champaign
+"""
