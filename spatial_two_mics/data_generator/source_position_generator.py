1e4eaf2da1083ff3dfea55e70d6290342155a511
Efthymios Tzinis <etzinis2@illinois.edu>
2018-09-02 13:42:53 -0500

    radomly generated taus time delays for both sources over reasonable physical distances for the room simulation

--- spatial_two_mics/data_generator/source_position_generator.py
+++ spatial_two_mics/data_generator/source_position_generator.py
@@ -53,20 +53,50 @@ class RandomCirclePositioner(object):
     def __init__(self,
                  min_angle=0.,
                  max_angle=np.pi,
-                 radius=1.0,
-                 mic_distance_percentage=0.001):
+                 radius=3.0,
+                 mic_distance_percentage=0.01,
+                 sound_speed=343,
+                 max_mixture_ratio=0.8,
+                 min_mixture_ratio=0.2,
+                 fs=16000):
+        """
+        :param min_angle: minimum angle in rads for the 2 sources
+        :param max_angle: maximum angle in rads for the 2 sources
+        :param radius: Radius of the circle in **meters**
+        :param mic_distance_percentage: Percentage of the radius
+        corresponding to the distance between the two microphones
+        :param sound_speed: Default 343 m/s in 20oC room temperature
+
+        :param max_mixture_ratio: m2(t) = a1*s1(t+d1) + a2*s2(t+d2)
+        :param min_mixture_ratio: a1 \in Uniform[.3, .7] and a2 = 1 - a1
+        :param fs: sampling ratio in Hz
+        """
 
         self.min_angle = min_angle
         self.max_angle = max_angle
         self.radius = radius
-        self.mic_distance = 2.0 * self.radius * mic_distance_percentage
+        self.mic_distance = self.radius * mic_distance_percentage
         self.m1 = (-self.mic_distance / 2, 0.)
         self.m2 = (self.mic_distance / 2, 0.)
+        self.sound_speed = sound_speed
+        self.min_mixture_ratio = min_mixture_ratio
+        self.max_mixture_ratio = max_mixture_ratio
+        self.fs = fs
 
     @staticmethod
     def get_cartessian_position(radius,
                                 angle):
         return radius * np.cos(angle), radius * np.sin(angle)
+
+    def get_time_delays_for_sources(self,
+                                    distances):
+        taus = {"tau1": (distances['s1m1']-distances['s1m2']),
+                "tau2": (distances['s2m1'] - distances['s2m2'])}
+        for tau in taus:
+            taus[tau] *= (1. * self.fs) / self.sound_speed
+            taus[tau] = int(taus[tau])
+
+        return taus
 
     def compute_distances_for_sources_and_mics(self, s1, s2):
         """! si must be in format (xi, yi)"""
@@ -103,9 +133,14 @@ class RandomCirclePositioner(object):
                 self.get_cartessian_position(self.radius, th_2))
                 for (th_1, th_2) in thetas]
 
-        distances = [self.compute_distances_for_sources_and_mics(s1,
-                                                                 s2)
+        distances = [self.compute_distances_for_sources_and_mics(s1, s2)
                      for (s1, s2) in xys]
+
+        taus = [self.get_time_delays_for_sources(loc_dists)
+                for loc_dists in distances]
+        print(taus)
+        # mix_amplitudes
+        # time_delays = sel
 
         sources_locations = [
                              {'thetas': thetas[i],
@@ -119,8 +154,7 @@ class RandomCirclePositioner(object):
 
 
 if __name__ == "__main__":
-    print("yolo ")
     random_positioner = RandomCirclePositioner()
     positions_info = random_positioner.get_sources_locations(2)
     from pprint import pprint
-    pprint(positions_info)
+    # pprint(positions_info)
