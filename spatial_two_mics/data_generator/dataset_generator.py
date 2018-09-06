db207f493dfefa781a1aee4e15c1a5909fd03308
Efthymios Tzinis <etzinis2@illinois.edu>
2018-09-05 19:07:09 -0500

    Truly random and tested data mixture generator from a dataset

--- spatial_two_mics/data_generator/dataset_generator.py
+++ spatial_two_mics/data_generator/dataset_generator.py
@@ -1,5 +1,6 @@
 """!
-@brief A dataset creation which is compatible with pytorch framework
+@brief A dataset creation which is used in order to combine the
+mixtures form the dataset and also store them inside a specified folder
 
 @author Efthymios Tzinis {etzinis2@illinois.edu}
 @copyright University of illinois at Urbana Champaign
@@ -13,18 +14,17 @@ from pprint import pprint
 from torch.utils.data import Dataset, DataLoader
 
 root_dir = os.path.join(
-           os.path.dirname(os.path.realpath(__file__)),
-           '../../')
+    os.path.dirname(os.path.realpath(__file__)),
+    '../../')
 sys.path.insert(0, root_dir)
 
 import spatial_two_mics.data_loaders.timit as timit_loader
 
 
-class PytorchCompatibleDataset(Dataset):
+class ArtificialDatasetCreator(object):
     """
-    This is a general compatible class for pytorch datasets all other
-    subclasses should inherit from this one and implement a sampler
-    in order to create the appropriate combinations of mixtures
+    This is a general compatible class for creating Artificial
+    mixtures with positions of the different sources.
     """
     def __init__(self,
                  audio_dataset_name="timit"):
@@ -35,18 +35,18 @@ class PytorchCompatibleDataset(Dataset):
                   "implemented.".format(audio_dataset_name))
 
 
-class RandomCombinations(PytorchCompatibleDataset):
+class RandomCombinations(ArtificialDatasetCreator):
     def __init__(self,
                  audio_dataset_name="timit",
-                 n_mixtures=0,
-                 n_mixed_sources=2,
                  genders_mixtures=None,
                  excluded_speakers=None,
                  subset_of_speakers='train'):
         super(RandomCombinations,
               self).__init__(audio_dataset_name=audio_dataset_name)
 
-        data_dic = self.data_loader.load()
+        self.data_dic = self.data_loader.load()
+        self.subset_of_speakers = subset_of_speakers
+
         if excluded_speakers is None:
             excluded_speakers = []
 
@@ -56,22 +56,15 @@ class RandomCombinations(PytorchCompatibleDataset):
         assert valid_genders, ('Valid genders for mixtures are f and m')
 
         self.used_speakers = self.get_available_speakers(
-                                  data_dic,
                                   subset_of_speakers,
                                   excluded_speakers)
 
-        mixture_combinations = self.get_mixture_combinations(
-                                    data_dic[subset_of_speakers],
-                                    n_mixed_sources=n_mixed_sources,
-                                    n_mixtures=n_mixtures)
-
     def get_available_speakers(self,
-                               data_dic,
                                subset_of_speakers,
                                excluded_speakers):
         try:
-            available_speakers = sorted(list(data_dic[
-                                 subset_of_speakers].keys()))
+            available_speakers = sorted(list(self.data_dic[
+                                                 subset_of_speakers].keys()))
         except KeyError:
             print("Subset: {} not available".format(subset_of_speakers))
             raise KeyError
@@ -80,8 +73,8 @@ class RandomCombinations(PytorchCompatibleDataset):
         for speaker in available_speakers:
 
             if ((speaker not in excluded_speakers) and
-                (data_dic[subset_of_speakers][speaker]['gender'] in
-                 self.genders_mixtures)):
+                    (self.data_dic[subset_of_speakers][
+                         speaker]['gender'] in self.genders_mixtures)):
                 valid_speakers.append(speaker)
 
         return valid_speakers
@@ -91,7 +84,7 @@ class RandomCombinations(PytorchCompatibleDataset):
         iter_len = len(iterable)
         max_combs = 1
         for i in np.arange(r):
-            max_combs *= (iter_len - i + 1) / (i+1)
+            max_combs *= (iter_len - i + 1) / (i + 1)
 
         already_seen = set()
         c = 0
@@ -136,15 +129,15 @@ class RandomCombinations(PytorchCompatibleDataset):
                 continue
 
             valid_mixtures.append(self.acquire_mixture_information(
-                                       speakers_dic,
-                                       possible_comb))
+                speakers_dic,
+                possible_comb))
 
         return valid_mixtures
 
     def get_mixture_combinations(self,
-                                 speakers_dic,
-                                 n_mixed_sources=2,
+                                 n_sources_in_mix=2,
                                  n_mixtures=0):
+        speakers_dic = self.data_dic[self.subset_of_speakers]
         possible_sources = []
         for speaker in self.used_speakers:
             sentences = list(speakers_dic[speaker]['sentences'].keys())
@@ -157,25 +150,33 @@ class RandomCombinations(PytorchCompatibleDataset):
         shuffle(possible_sources)
 
         valid_combinations = self.get_only_valid_mixture_combinations(
-                                  possible_sources,
-                                  speakers_dic,
-                                  n_mixed_sources=n_mixed_sources,
-                                  n_mixtures=n_mixtures)
-
-
+            possible_sources,
+            speakers_dic,
+            n_mixed_sources=n_sources_in_mix,
+            n_mixtures=n_mixtures)
 
         mixtures = []
-
+        print(len(valid_combinations))
         input()
         return mixtures
 
 
+def example_of_usage():
+    """!
+    Creates a list of mixtures in appropriate format with all the
+    information that might be needed next"""
+
+    timit_mixture_creator = RandomCombinations(
+                            audio_dataset_name="timit",
+                            genders_mixtures=['m', 'f'],
+                            subset_of_speakers='test',
+                            excluded_speakers=['mwew0'])
+
+    mixture_combinations = timit_mixture_creator.get_mixture_combinations(
+                           n_sources_in_mix=3,
+                           n_mixtures=10000)
+
+
 if __name__ == "__main__":
-    timit_random_combs = RandomCombinations(
-                         audio_dataset_name="timit",
-                         genders_mixtures=['m', 'f'],
-                         subset_of_speakers='test',
-                         n_mixtures=10,
-                         n_mixed_sources=4,
-                         excluded_speakers=['mwew0'])
+    example_of_usage()
 
