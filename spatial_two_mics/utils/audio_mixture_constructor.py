27999eedcb9050a3615799ea41644ef0bea80bf4
Efthymios Tzinis <etzinis2@illinois.edu>
2018-09-07 14:25:37 -0500

    appropriate usage of example for mixture information

--- spatial_two_mics/utils/audio_mixture_constructor.py
+++ spatial_two_mics/utils/audio_mixture_constructor.py
@@ -179,64 +179,24 @@ class AudioMixtureConstructor(object):
         return tf_representations
 
 
-def mixture_info_example():
+def example_of_usage():
+    """!
+    How the class of Audio mixtures should be called"""
+
     import os
     import sys
     root_dir = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         '../../')
     sys.path.insert(0, root_dir)
-
-    from spatial_two_mics.config import TIMIT_PATH
-
-    ex = {'positions':
-                  {'amplitudes': np.array([0.73382382,
-                                                0.26617618]),
-                   'd_thetas': np.array([1.06829948]),
-                   'distances': {'m1m1': 0.0,
-                                 'm1m2': 0.03,
-                                 'm1s1': 3.015,
-                                 'm1s2': 3.0072529608785676,
-                                 'm2m1': 0.03,
-                                 'm2m2': 0.0,
-                                 'm2s1': 2.985,
-                                 'm2s2': 2.9928046426867034,
-                                 's1m1': 3.015,
-                                 's1m2': 2.985,
-                                 's1s1': 0.0,
-                                 's1s2': 3.054656422155759,
-                                 's2m1': 3.0072529608785676,
-                                 's2m2': 2.9928046426867034,
-                                 's2s1': 3.054656422155759,
-                                 's2s2': 0.0},
-                   'taus': np.array([1.39941691, 0.67397403]),
-                   'thetas': np.array([0., 1.06829948]),
-                   'xy_positons': np.array([[3., 0.],
-                                         [1.44484569, 2.62914833]])},
-     'sources_ids': [{'gender': 'f',
-                     'sentence_id': 'sa1',
-                     'speaker_id': 'flbw0',
-                     'wav_path': os.path.join(TIMIT_PATH,
-                                 'test/dr4/flbw0/sa1.wav')},
-                    {'gender': 'm',
-                     'sentence_id': 'sa2',
-                     'speaker_id': 'mbns0',
-                     'wav_path': os.path.join(TIMIT_PATH,
-                                 'test/dr4/mbns0/sa2.wav')}
-                     ]}
-
-    return ex
-
-def example_of_usage():
-    """!
-    How the class of Audio mixtures should be called"""
+    import spatial_two_mics.examples.mixture_example as me
 
     mixture_creator = AudioMixtureConstructor(n_fft=1024,
                                               win_len=1024,
                                               hop_len=512,
                                               mixture_duration=2.0)
 
-    example_mixture_info = mixture_info_example()
+    example_mixture_info = me.mixture_info_example()
 
     tf_mixtures = mixture_creator.construct_mixture(
                                   example_mixture_info,
