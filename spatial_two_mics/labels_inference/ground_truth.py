1e4333d5282dadeab53548309ee17d761fe738a6
Efthymios Tzinis <etzinis2@illinois.edu>
2018-09-07 19:04:39 -0500

    Ground truth assertions for avoiding errors

---
+++ spatial_two_mics/labels_inference/ground_truth.py
@@ -0,0 +1,57 @@
+"""!
+@brief Infering the masking for eah tf bin independently based on the
+maximum energy of the sources in each bin
+
+@author Efthymios Tzinis {etzinis2@illinois.edu}
+@copyright University of Illinois at Urbana Champaign
+"""
+
+import numpy as np
+
+
+def infer_mask(mixture_info):
+    """
+    :param mixture_info:
+    mixture_info = {
+        'm1_raw': numpy array containing the raw m1 signal,
+        'm2_raw': numpy array containing the raw m2 signal,
+        'm1_tf': numpy array containing the m1 TF representation,
+        'm2_tf': numpy array containing the m2 TF representation,
+        'sources_raw': a list of numpy 1d vectors containing the
+        sources ,
+        'sources_tf': a list of numpy 2d vectors containing the
+         TF represeantations of the sources ,
+        'delayed_sources_raw': a list of numpy 1d vectors containing
+        the sources delayed with some tau,
+        'delayed_sources_tf': a list of numpy 2d vectors
+        containing the TF representations of the delayed signals,
+        'amplitudes': the weights that each source contributes to
+        the mixture of the second microphone
+    }
+
+    :return: A tf 2d matrix corresponding to the dominating source
+    for each TF bin [0,1,...,n_sources]
+    """
+    sources_complex_spectra = mixture_info['sources_tf']
+    amplitudes = mixture_info['amplitudes']
+    n_sources = len(sources_complex_spectra)
+
+    assert len(amplitudes) == n_sources, "Length of weights: {} " \
+                                         "should be equal to the " \
+                                         "number of sources: {}" \
+                                         "".format(len(amplitudes),
+                                                   n_sources)
+
+    same_dimensions = [(sources_complex_spectra[i].shape ==
+                        sources_complex_spectra[0].shape)
+                       for i in np.arange(len(sources_complex_spectra))]
+
+    assert all(same_dimensions), "All arrays should have the same " \
+                                 "dimensions. However, got sizes of {}"\
+                                 "".format([x.shape for x in
+                                            sources_complex_spectra])
+
+    return "Psolos"
+    # mixture_tensor = np.dstack()
+
+
