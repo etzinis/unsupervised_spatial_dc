731a8d3b6dde68f480a7cd9cb8111f4498a08fa5
Efthymios Tzinis <etzinis2@illinois.edu>
2018-09-07 19:04:03 -0500

    General class for estimating the labels for each TF usingdifferent algorithms or even ground truth

--- spatial_two_mics/labels_inference/tf_label_estimator.py
+++ spatial_two_mics/labels_inference/tf_label_estimator.py
@@ -14,6 +14,7 @@ root_dir = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     '../../')
 sys.path.insert(0, root_dir)
+import spatial_two_mics.labels_inference.ground_truth as gt_inference
 
 
 class TFMaskEstimator(object):
@@ -25,18 +26,66 @@ class TFMaskEstimator(object):
     def __init__(self,
                  inference_method=None):
         if inference_method.lower() == "ground_truth":
-            print("Build fucking ground truth first")
+            self.label_inference = gt_inference
         else:
             raise NotImplementedError("Inference Method: {} is not yet "
                   "implemented.".format(inference_method))
+
+    def infer_mixture_labels(self,
+                             mixture_info):
+        """
+        :param mixture_info:
+        mixture_info = {
+            'm1_raw': numpy array containing the raw m1 signal,
+            'm2_raw': numpy array containing the raw m2 signal,
+            'm1_tf': numpy array containing the m1 TF representation,
+            'm2_tf': numpy array containing the m2 TF representation,
+            'sources_raw': a list of numpy 1d vectors containing the
+            sources ,
+            'sources_tf': a list of numpy 2d vectors containing the
+             TF represeantations of the sources ,
+            'delayed_sources_raw': a list of numpy 1d vectors containing
+            the sources delayed with some tau,
+            'delayed_sources_tf': a list of numpy 2d vectors
+            containing the TF representations of the delayed signals,
+            'amplitudes': the weights that each source contributes to
+            the mixture of the second microphone
+        }
+
+        :return: A TF representation with each TF bin to correspond
+        to the source which the algorithm predicts that is dominating
+        """
+
+        return self.label_inference.infer_mask(mixture_info)
 
 
 def example_of_usage():
     """!
     How the class of Audio mixtures should be called"""
 
+    import os
+    import sys
+    root_dir = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '../../')
+    sys.path.insert(0, root_dir)
+    import spatial_two_mics.examples.mixture_example as me
+    import spatial_two_mics.utils.audio_mixture_constructor as \
+        mix_constructor
+
+    mixture_info = me.mixture_info_example()
+    mixture_creator = mix_constructor.AudioMixtureConstructor(
+        n_fft=1024, win_len=400, hop_len=200, mixture_duration=2.0,
+        force_all_signals_one_sample_delay=True)
+
+    tf_mixtures = mixture_creator.construct_mixture(mixture_info)
+
     ground_truth_estimator = TFMaskEstimator(
                              inference_method='Ground_truth')
+
+    tf_labels = ground_truth_estimator.infer_mixture_labels(tf_mixtures)
+    pprint(tf_labels)
+
 
 if __name__ == "__main__":
     example_of_usage()
