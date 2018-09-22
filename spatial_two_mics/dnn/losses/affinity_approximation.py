c87384d4308b4cc829e83763ba1a3d84b8791c02
Efthymios Tzinis <etzinis2@illinois.edu>
2018-09-22 16:29:33 -0500

    Affinity loss with computing the diagonal affinities

--- spatial_two_mics/dnn/losses/affinity_approximation.py
+++ spatial_two_mics/dnn/losses/affinity_approximation.py
@@ -6,6 +6,8 @@ affinity mask
 @copyright University of illinois at Urbana Champaign
 """
 import torch
+import torch.nn as nn
+
 
 def naive(vs, ys):
     """! Computing naively the loss function between embedding
@@ -24,3 +26,31 @@ def naive(vs, ys):
          + torch.sqrt(torch.mean(torch.matmul(
            ys.transpose(1, 2), ys) ** 2))
     return loss / vs.size(0)
+
+
+def diagonal(embedding, assignments):
+    batch_size, sequence_length, num_frequencies, embedding_size = embedding.size()
+    _, _, _, num_sources = assignments.size()
+    embedding = embedding.view(-1, embedding.size()[-1])
+    assignments = assignments.view(-1, assignments.size()[-1])
+
+    class_weights = nn.functional.normalize(torch.sum(assignments.detach(), dim=-2), p=1, dim=-1).unsqueeze(0)
+    class_weights = 1.0 / (torch.sqrt(class_weights) + 1e-7)
+    weights = torch.matmul(assignments.detach(), class_weights.transpose(1, 0))
+    norm = torch.sum(weights**2)**2
+    assignments = assignments * weights.repeat(1, assignments.size()[-1])
+    embedding = embedding * weights.repeat(1, embedding.size()[-1])
+
+    embedding = embedding.view(batch_size, sequence_length*num_frequencies, embedding_size)
+    assignments = assignments.view(batch_size, sequence_length*num_frequencies, num_sources)
+
+    embedding_transpose = embedding.permute(0, 2, 1)
+    assignments_transpose = assignments.permute(0, 2, 1)
+
+    loss_est = torch.sum(torch.matmul(embedding_transpose, embedding)**2)
+    loss_est_true = torch.sum(torch.matmul(embedding_transpose, assignments)**2)
+    loss_true = torch.sum(torch.matmul(assignments_transpose, assignments)**2)
+    loss = loss_est - 2*loss_est_true + loss_true
+    loss = loss / norm
+    return loss
+
