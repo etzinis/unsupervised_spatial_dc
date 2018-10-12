28365078aad26672384864208cddef91a06ba764
Efthymios Tzinis <etzinis2@illinois.edu>
2018-10-12 13:17:56 -0500

    New experiment parser ofcommand line arguments which is much more user friendly

--- spatial_two_mics/dnn/utils/experiment_command_line_parser_v2.py
+++ spatial_two_mics/dnn/utils/experiment_command_line_parser_v2.py
@@ -13,26 +13,26 @@ def get_args():
                                                  'Audio Source '
                                                  'Separation '
                                                  'Experiment')
-    parser.add_argument("--dataset", type=str,
-                        help="Dataset name",
-                        default="timit")
-    parser.add_argument("--n_sources", type=int,
-                        help="How many sources in each mix",
-                        default=2)
-    parser.add_argument("--n_samples", type=int, nargs='+',
-                        help="How many samples do u want to be "
-                             "created for train test val",
-                        default=[256, 64, 128])
-    parser.add_argument("--genders", type=str, nargs='+',
-                        help="Genders that will correspond to the "
-                             "genders in the mixtures",
-                        default=['m'])
-    parser.add_argument("-f", "--force_delays", nargs='+', type=int,
-                        help="""Whether you want to force integer 
-                        delays of +- 1 in the sources e.g.""",
-                        default=[-1, 1])
+    parser.add_argument("--train", type=str,
+                        help="Path for the training dataset",
+                        default=None)
+    parser.add_argument("--test", type=str,
+                        help="Path for the testing dataset",
+                        default=None)
+    parser.add_argument("--val", type=str,
+                        help="Path for the validation dataset",
+                        default=None)
+    parser.add_argument("--n_train", type=int,
+                        help="""Reduce the number of training 
+                            samples to this number.""", default=None)
+    parser.add_argument("--n_test", type=int,
+                        help="""Reduce the number of testing 
+                            samples to this number.""", default=None)
+    parser.add_argument("--n_val", type=int,
+                        help="""Reduce the number of evaluation 
+                            samples to this number.""", default=None)
     parser.add_argument("-nl", "--n_layers", type=int,
-                        help="""The number of layers of the LSTM 
+                        help="""The number of layers of the BLSTM 
                         encoder""", default=2)
     parser.add_argument("-ed", "--embedding_depth", type=int,
                         help="""The depth of the embedding""",
@@ -41,16 +41,18 @@ def get_args():
                         help="""The size of the LSTM cells """,
                         default=10)
     parser.add_argument("-bs", "--batch_size", type=int,
-                        help="""The number of samples in each batch""",
-                        default=64)
+                        help="""The number of samples in each batch. 
+                        Warning: Cannot be less than the number of 
+                        the validation samples""", default=64)
     parser.add_argument("-name", "--experiment_name", type=str,
                         help="""The name or identifier of this 
                         experiment""",
                         default='A sample experiment'),
-    parser.add_argument("-mt", "--labels_mask", type=str,
+    parser.add_argument("-l", "--labels_mask", type=str,
                         help="""The type of masks that you want to 
                         use -- 'ground_truth' or 'duet'""",
-                        default='duet')
+                        default='duet', choices=['duet',
+                                                 'ground_truth'])
     parser.add_argument("-cad", "--cuda_available_devices", type=int,
                         nargs="+",
                         help="""A list of Cuda IDs that would be 
@@ -62,15 +64,18 @@ def get_args():
     parser.add_argument("--epochs", type=int,
                         help="""The number of epochs that the 
                         experiment should run""", default=50)
-    parser.add_argument("--evaluate_per", type=int,
-                        help="""The number of trianing epochs in 
+    parser.add_argument("--eval_per", type=int,
+                        help="""The number of training epochs in 
                         order to run an evaluation""", default=5)
-    parser.add_argument("--n_eval", type=int,
-                        help="""Reduce the number of eavluation 
-                        samples to this number.""", default=256)
     parser.add_argument("-lr", "--learning_rate", type=float,
                         help="""Initial Learning rate""", default=1e-3)
     parser.add_argument("--bidirectional", action='store_true',
                         help="""Bidirectional or not""")
+    parser.add_argument("--save_best", type=int,
+                        help="""The number of best models dependent 
+                        on the metric you want to use that are going 
+                        to be saved under the preferred logging model 
+                        directory.""",
+                        default=10)
 
     return parser.parse_args()
