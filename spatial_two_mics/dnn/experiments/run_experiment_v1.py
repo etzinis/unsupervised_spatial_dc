ae3396636c0a9114170004e28eb23fbdc470777d
Efthymios Tzinis <etzinis2@illinois.edu>
2018-10-15 04:23:47 -0500

    Run experiment in a new formatwithall the new arguments and saving the best models

--- spatial_two_mics/dnn/experiments/run_experiment_v1.py
+++ spatial_two_mics/dnn/experiments/run_experiment_v1.py
@@ -26,37 +26,32 @@ sys.path.insert(0, root_dir)
 import spatial_two_mics.dnn.models.simple_LSTM_encoder as LSTM_enc
 import spatial_two_mics.dnn.losses.affinity_approximation as \
     affinity_losses
-import spatial_two_mics.dnn.utils.fast_dataset_v2 as fast_data_gen
+import spatial_two_mics.dnn.utils.fast_dataset_v3 as fast_data_gen
 import spatial_two_mics.dnn.utils.data_conversions as converters
 import spatial_two_mics.dnn.utils.experiment_command_line_parser_v2 as \
     parser
 import spatial_two_mics.dnn.utils.update_history as update_history
+import spatial_two_mics.dnn.utils.model_logger as model_logger
 from progress.bar import ChargingBar
 import spatial_two_mics.dnn.evaluation.naive_evaluation_numpy as \
     numpy_eval
 from sklearn.cluster import KMeans
 
 
-def train(args,
-          model,
+def train(model,
           training_generator,
           optimizer,
           mean_tr,
           std_tr,
           epoch,
           history,
-          n_batches):
+          n_batches,
+          n_sources):
     model.train()
-    timing_dic = {'Loading batch': 0.,
-                  'Transformations and Forward': 0.,
-                  'Loss Computation and Backprop': 0.}
-    before = time.time()
     bar = ChargingBar("Training for epoch: {}...".format(epoch),
                       max=n_batches)
     for batch_data in training_generator:
         (abs_tfs, masks) = batch_data
-        timing_dic['Loading batch'] += time.time() - before
-        before = time.time()
         input_tfs, index_ys = abs_tfs.cuda(), masks.cuda()
         # the input sequence is determined by time and not freqs
         # before: input_tfs = batch_size x (n_fft/2+1) x n_timesteps
@@ -69,62 +64,44 @@ def train(args,
 
         # index_ys = index_ys.permute(0, 2, 1).contiguous()
         one_hot_ys = converters.one_hot_3Dmasks(index_ys,
-                                                args.n_sources)
-
-        optimizer.zero_grad()
-        vs = model(input_tfs)
-
+                                                n_sources)
         flatened_ys = one_hot_ys.view(one_hot_ys.size(0),
                                       -1,
                                       one_hot_ys.size(-1)).cuda()
 
-        timing_dic['Transformations and Forward'] += time.time() - \
-                                                     before
-        before = time.time()
+        optimizer.zero_grad()
+        vs = model(input_tfs)
+
+
         loss = affinity_losses.paris_naive(vs, flatened_ys)
-        # loss = affinity_losses.diagonal(vs.view(vs.size(0),
-        #                                         one_hot_ys.size(1),
-        #                                         one_hot_ys.size(2),
-        #                                         vs.size(-1)),
-        #                                 one_hot_ys.cuda())
 
         loss.backward()
-        nn.utils.clip_grad_norm(model.parameters(), 100.)
+        nn.utils.clip_grad_norm_(model.parameters(), 100.)
         optimizer.step()
-        timing_dic['Loss Computation and Backprop'] += time.time() - \
-                                                       before
 
         update_history.values_update([('loss', loss)],
                                      history, update_mode='batch')
-        before = time.time()
         bar.next()
     bar.finish()
 
-    pprint(timing_dic)
 
-
-def eval(args,
-         model,
+def eval(model,
          val_generator,
          mean_tr,
          std_tr,
          epoch,
          history,
          n_batches,
-         k_means_obj):
-    timing_dic = {'Standard Scaler': 0.,
-                  'Kmeans': 0.,
-                  'Dummy BSS evaluation': 0.}
+         k_means_obj,
+         n_sources,
+         batch_size):
 
-    # make some evaluation
     model.eval()
-    before = time.time()
     with torch.no_grad():
         bar = ChargingBar("Evaluating for epoch: {}...".format(epoch),
-                          max=n_batches)
-        before = time.time()
+                          max=n_batches*batch_size)
         for batch_data in val_generator:
-            abs_tfs, masks, wavs_lists, real_tfs, imag_tfs = batch_data
+            abs_tfs, wavs_lists, real_tfs, imag_tfs = batch_data
             input_tfs = abs_tfs.cuda()
             # the input sequence is determined by time and not freqs
             # before: input_tfs = batch_size x (n_fft/2+1) x n_timesteps
@@ -137,35 +114,18 @@ def eval(args,
             vs = model(input_tfs)
             for b in np.arange(vs.size(0)):
 
-                # possibly go into GPU ?
-                # before = time.time()
-                # embedding_features = z_scaler.fit_transform(
-                #     vs[b, :, :].data.cpu().numpy())
-                # timing_dic['Standard Scaler'] += time.time() - before
-
                 embedding_features = vs[b, :, :].data.cpu().numpy()
-                # embedding_features = masks[b, :, :].view(-1, 1).data.numpy()
-                # embedding_labels = masks[b].data.numpy()
-                # embedding_features = flatened_ys[b, :, :].data.cpu().numpy()
 
-
-
-                # possibly perform kmeans on GPU?
-                before = time.time()
                 embedding_labels = np.array(k_means_obj.fit_predict(
                                             embedding_features))
-                timing_dic['Kmeans'] += time.time() - before
 
-                # possibly do it on GPU?
-                before = time.time()
                 sdr, sir, sar = numpy_eval.naive_cpu_bss_eval(
                     embedding_labels,
                     real_tfs[b].data.numpy(),
                     imag_tfs[b].data.numpy(),
                     wavs_lists[b].data.numpy(),
-                    args.n_sources,
+                    n_sources,
                     batch_index=b)
-                timing_dic['Dummy BSS evaluation'] += time.time() - before
 
                 update_history.values_update([('sdr', sdr),
                                               ('sir', sir),
@@ -173,8 +133,7 @@ def eval(args,
                                              history,
                                              update_mode='batch')
 
-            bar.next()
-        pprint(timing_dic)
+                bar.next()
         bar.finish()
 
 
@@ -182,42 +141,60 @@ def run_LSTM_experiment(args):
     visible_cuda_ids = ','.join(map(str, args.cuda_available_devices))
     os.environ["CUDA_VISIBLE_DEVICES"] = visible_cuda_ids
 
-    (training_generator, mean_tr, std_tr, n_tr_batches) = \
-        fast_data_gen.get_data_generator(args,
-                                         return_stats=True)
+    (training_generator, mean_tr, std_tr, n_tr_batches, n_tr_sources) =\
+    fast_data_gen.get_data_generator(args.train,
+                                     partition='train',
+                                     num_workers=args.num_workers,
+                                     return_stats=True,
+                                     get_top=args.n_train,
+                                     batch_size=args.batch_size,
+                                     return_n_batches=True,
+                                     labels_mask=args.training_labels,
+                                     return_n_sources=True)
 
-    val_args = copy.copy(args)
-    val_args.partition = 'val'
-    val_generator, n_val_batches = \
-        fast_data_gen.get_data_generator(val_args,
-                                         get_top=args.n_eval)
+    val_generator, n_val_batches, n_val_sources = \
+    fast_data_gen.get_data_generator(args.val,
+                                     partition='val',
+                                     num_workers=args.num_workers,
+                                     return_stats=False,
+                                     get_top=args.n_val,
+                                     batch_size=args.batch_size,
+                                     return_n_batches=True,
+                                     labels_mask=None,
+                                     return_n_sources=True)
 
     model = LSTM_enc.BLSTMEncoder(num_layers=args.n_layers,
                                   hidden_size=args.hidden_size,
                                   embedding_depth=args.embedding_depth,
-                                  bidirectional=args.bidirectional)
+                                  bidirectional=args.bidirectional,
+                                  dropout=args.dropout)
     model = nn.DataParallel(model).cuda()
 
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=args.learning_rate,
                                  betas=(0.9, 0.999))
 
-    k_means_obj = KMeans(n_clusters=2)
+    assert n_val_sources == n_tr_sources, "Number of sources in both " \
+                                          "training and evaluation " \
+                                          "should be equal while " \
+                                          "training"
+    k_means_obj = KMeans(n_clusters=n_tr_sources)
     # just iterate over the data
     history = {}
     for epoch in np.arange(args.epochs):
 
-        train(args, model, training_generator, optimizer, mean_tr,
-              std_tr, epoch, history, n_tr_batches)
+        train(model, training_generator, optimizer, mean_tr,
+              std_tr, epoch, history, n_tr_batches, n_tr_sources)
 
         update_history.values_update([('loss', None)],
                                      history,
                                      update_mode='epoch')
 
 
-        if epoch % args.evaluate_per == 0:
-            eval(args, model, val_generator, mean_tr,
-                 std_tr, epoch, history, n_val_batches, k_means_obj)
+        if epoch % args.eval_per == 0:
+            eval(model, val_generator, mean_tr, std_tr, epoch,
+                 history, n_val_batches, k_means_obj, n_val_sources,
+                 args.batch_size)
 
             update_history.values_update([('sdr', None),
                                           ('sir', None),
@@ -225,16 +202,45 @@ def run_LSTM_experiment(args):
                                          history,
                                          update_mode='epoch')
 
+            # keep track of best performances so far
+            epoch_performance_dic = {
+                'sdr': history['sdr'][-1],
+                'sir': history['sir'][-1],
+                'sar': history['sar'][-1]
+            }
+            update_history.update_best_performance(
+                           epoch_performance_dic, epoch, history,
+                           buffer_size=args.save_best)
+
+
+            # save the model if it is one of the best according to SDR
+            if (history['sdr'][-1] >=
+                history['best_performances'][-1][0]['sdr']):
+                dataset_id = os.path.basename(args.train)
+
+                model_logger.save(model,
+                                  optimizer,
+                                  args,
+                                  epoch,
+                                  epoch_performance_dic,
+                                  dataset_id)
+
+                # model, optimizer, epoch, val_performance, new_args = \
+                # model_logger.load(model, optimizer, dataset_id,
+                #                   filename=None)
+
+
         pprint(history['loss'][-1])
         pprint(history['sdr'][-1])
         pprint(history['sir'][-1])
         pprint(history['sar'][-1])
-        print(
-            "BEST SDR: {}, SIR: {}, SAR {}".format(max(history['sdr']),
-                                                   max(history['sir']),
-                                                   max(history['sar'])))
+        pprint(history['best_performances'])
+        # print(
+        #     "BEST SDR: {}, SIR: {}, SAR {}".format(max(history['sdr']),
+        #                                            max(history['sir']),
+        #                                            max(history['sar'])))
 
 
 if __name__ == "__main__":
     args = parser.get_args()
-    # run_LSTM_experiment(args)
+    run_LSTM_experiment(args)
