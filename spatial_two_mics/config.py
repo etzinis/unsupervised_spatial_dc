802081e110d3e62ee21105c752f419e2b2e9d96d
Efthymios Tzinis <etzinis2@illinois.edu>
2018-10-15 03:09:59 -0500

    Adding generalmodels directory in order to be in the config file

--- spatial_two_mics/config.py
+++ spatial_two_mics/config.py
@@ -4,4 +4,5 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TIMIT_PATH = "/mnt/data/Speech/timit-wav"
-DATASETS_DIR = "/home/thymios/data/"
+DATASETS_DIR = "/mnt/nvme/spatial_two_mics_data/"
+MODELS_DIR = "/mnt/nvme/spatial_two_mics_models/"
