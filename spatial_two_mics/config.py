e99fb389733d1c80fc4144a13531590a4a16b088
Efthymios Tzinis <etzinis2@illinois.edu>
2018-10-15 18:51:04 -0500

    Config for the resuilts of the evaluation of the pretrained models

--- spatial_two_mics/config.py
+++ spatial_two_mics/config.py
@@ -6,3 +6,4 @@ BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TIMIT_PATH = "/mnt/data/Speech/timit-wav"
 DATASETS_DIR = "/mnt/nvme/spatial_two_mics_data/"
 MODELS_DIR = "/mnt/nvme/spatial_two_mics_models/"
+MODELS_DIR = "/mnt/nvme/spatial_two_mics_results/"
